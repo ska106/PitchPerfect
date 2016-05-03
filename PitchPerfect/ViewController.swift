8f4696d5064615eb4e4896cc2ee6640f2c96cd1e
Sudeep Agrawal <Sudeep.Agrawal@gmail.com>
2016-05-03 01:24:48 -0400

    Added UI Control buttons

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject)
+    {
+        print("Record buttons was pressed");
+        recordingLabel.text="Recoding in Progress !!";
+    }
 
+    @IBAction func stopRecording(sender: AnyObject)
+    {
+        print("Stop Recording button was pressed");
+    }
 }
 
